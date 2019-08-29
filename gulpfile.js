eb1c680071b28eb0edd277850ca1b5bb9c344982
hamishgray <hamish.gray@secretescapes.com>
2019-08-29 17:12:11 +0100

    first review

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,6 @@ function buildJsMain(cb) {
     // plugins
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/jquery-countdown/dist/jquery.countdown.min.js',
-    // './node_modules/waypoints/lib/jquery.waypoints.min.js',
 
     // custom js - with on doc ready wrapper
     './_assets/js/_components/on-ready/start.js',
@@ -87,7 +86,7 @@ function buildJsMain(cb) {
     './_assets/js/_components/standard.js',
     './_assets/js/_components/offer-countdown.js',
     './_assets/js/_components/modal.js',
-    // './_assets/js/_components/sticky-nav.js',
+    './_assets/js/_components/sticky-nav.js',
     './_assets/js/_components/form/functions.js',
     './_assets/js/_components/form/validation.js',
     './_assets/js/_components/competition.js',
